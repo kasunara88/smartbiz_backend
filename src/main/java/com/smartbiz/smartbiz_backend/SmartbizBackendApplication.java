cb8afd711b8227a37e3f67d0c9b58760d7c9d4f1
kasunara88 <wmaravinda@gmail.com>
2025-10-05 21:27:19 +0530

    first commit

---
+++ src/main/java/com/smartbiz/smartbiz_backend/SmartbizBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smartbiz.smartbiz_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartbizBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartbizBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartbiz/smartbiz_backend/SmartbizBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartbiz.smartbiz_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartbizBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
